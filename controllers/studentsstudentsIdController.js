458346c798721ebb41c9bb1d933698e7dabdc06a
shyamraja <t7bhsh00@students.oamk.fi>
2019-01-31 14:49:51 +0200

    init project

---
+++ controllers/studentsstudentsIdController.js
@@ -0,0 +1,48 @@
+'use strict'
+
+var varstudentsstudentsIdController = require('./studentsstudentsIdControllerService');
+
+module.exports.funcstudentsstudentsIdPARAMETERS = function funcstudentsstudentsIdPARAMETERS(req, res, next) {
+  varstudentsstudentsIdController.funcstudentsstudentsIdPARAMETERS(req.swagger.params, res, next);
+};
+
+module.exports.getStudents = function getStudents(req, res, next) {
+  varstudentsstudentsIdController.getStudents(req.swagger.params, res, next);
+};
+
+module.exports.updateStudents = function updateStudents(req, res, next) {
+  varstudentsstudentsIdController.updateStudents(req.swagger.params, res, next);
+};
+
+module.exports.deleteStudents = function deleteStudents(req, res, next) {
+  varstusentsstudentsIdController.deleteStudents(req.swagger.params, res, next);
+};
+  
+
+module.exports.funcstudentsstudentsIdPOST = function funcstudentsstudentsIdPOST(req, res, next) {
+  varstudentsstudentsIdController.funcstudentsstudentsIdPOST(req.swagger.params, res, next);
+};
+'use strict'
+
+var varstudentsstudentsIdController = require('./studentsstudentsIdControllerService');
+
+module.exports.funcstudentsstudentsIdPARAMETERS = function funcstudentsstudentsIdPARAMETERS(req, res, next) {
+  varstudentsstudentsIdController.funcstudentsstudentsIdPARAMETERS(req.swagger.params, res, next);
+};
+
+module.exports.getStudents = function getStudents(req, res, next) {
+  varstudentsstudentsIdController.getStudents(req.swagger.params, res, next);
+};
+
+module.exports.updateStudents = function updateStudents(req, res, next) {
+  
+}
+module.exports.deleteStudents = function deleteStudents(req, res, next) {
+  varstudentsstudentsIdController.deleteStudents(req.swagger.params, res, next);
+};
+
+'use strict'
+
+module.exports.funcstudentsstudentsIdPOST = function funcstudentsstudentsIdPOST(req, res, next) {
+  varstudentsstudentsIdController.funcstudentsstudentsIdPOST(req.swagger.params, res, next);
+};
