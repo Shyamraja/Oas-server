4f6f4e3cdd823d3a74e2192e12bb911b78e7a755
shyamraja <t7bhsh00@students.oamk.fi>
2019-01-31 14:56:00 +0200

    Test git version controll 2

--- controllers/gradesgradesIdController.js
+++ controllers/gradesgradesIdController.js
@@ -29,3 +29,10 @@ module.exports.getGrades = function getGrades(req, res, next) {
   vargradesgradesIdController.getGrades(req.swagger.params, res, next);
 };
 
+module.exports.updateGrades = function updateGrades(req, res, next) {
+  vargradesgradesIdController.updateGrades(req.swagger.params, res, next);
+};
+
+module.exports.deleteGrades = function deleteGrades(req, res, next) {
+  vargradesgradesIdController.deleteGrades(req.swagger.params, res, next);
+};
