2f161b7e11730df5907176af5c6a101269bb3603
shyamraja <t7bhsh00@students.oamk.fi>
2019-02-08 00:21:13 +0200

    trying to fix functions for CRUD operations

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var varstudentsController = require('./studentsControllerService');
 
@@ -11,20 +11,20 @@ module.exports.createStudents = function createStudents(req, res, next) {
 };
 
 module.exports.funcstudentsPUT = function funcstudentsPUT(req, res, next) {
-  varstudentsController.funcstudentsPUT(req.swagger.params, res, next);
+varstudentsController.funcstudentsPUT(req.swagger.params, res, next);
 };
-'use strict'
+//'use strict'
 
-var varstudentsController = require('./studentsControllerService');
+//var varstudentsController = require('./studentsControllerService');
 
-module.exports.findStudents = function findStudents(req, res, next) {
-  varstudentsController.findStudents(req.swagger.params, res, next);
-};
+//module.exports.findStudents = function findStudents(req, res, next) {
+//  varstudentsController.findStudents(req.swagger.params, res, next);
+//};
 
-module.exports.createStudents = function createStudents(req, res, next) {
-  varstudentsController.createStudents(req.swagger.params, res, next);
-};
+//module.exports.createStudents = function createStudents(req, res, next) {
+ // varstudentsController.createStudents(req.swagger.params, res, next);
+//};
 
-module.exports.funcstudentsPUT = function funcstudentsPUT(req, res, next) {
-  varstudentsController.funcstudentsPUT(req.swagger.params, res, next);
-};
+//module.exports.funcstudentsPUT = function funcstudentsPUT(req, res, next) {
+ // varstudentsController.funcstudentsPUT(req.swagger.params, res, next);
+//};
