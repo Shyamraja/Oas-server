ca5d6350e695eb24470873200fb46d7fa27b706c
shyamraja <t7bhsh00@students.oamk.fi>
2019-02-08 00:59:09 +0200

    functions updated for studentId controllers

--- controllers/studentsstudentsIdControllerService.js
+++ controllers/studentsstudentsIdControllerService.js
@@ -1,62 +1,44 @@
-'use strict'
-
-module.exports.funcstudentsstudentsIdPARAMETERS = function funcstudentsstudentsIdPARAMETERS(req, res, next) {
+'use strict';
+let students = require('./students');
+module.exports.funcstudentsstudentIDPARAMETERS = function funcstudentsstudentIDPARAMETERS(req, res, next) {
   res.send({
-    message: 'This is the mockup controller for funcstudentsstudentsIdPARAMETERS'
+      message: 'This is the mockup controller for funcstudentsstudentIDPARAMETERS'
   });
 };
 
 module.exports.getStudents = function getStudents(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for getStudents'
-  });
+   const id = req.studentId.value;
+   console.log("get student id", id);
+    const student = students.getStudent(id);
+    if (student) {
+      res.status(200).json(student);
+  } else {
+      res.status(404).send('STUDENT NOT FOUND');
+  }
 };
+  //  message: 'This is the mockup controller for getStudents'
+ // });
+//};
 
 module.exports.updateStudents = function updateStudents(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for updateStudents'
-  });
-};
-
+ console.log("put students req",req);
+ let student = req.undefined.value;
+ console.log("put students", student);
+ if(students.putStudent(student)) {
+   res.status(200).send("UPDATED");
+  }else{
+   res.status(400).send("NOT UPDATED");
+       }
+ };
 module.exports.deleteStudents = function deleteStudents(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for deleteStudents'
-  });
-};
+  //res.send({
+    //message: 'This is the mockup controller for deleteStudents'
+  const deleteId = req.studentId.value;
+  if (students.deleteStudent(deleteId)){
+    res.status(200).send("Student Delete OK");
+  } else{
+  res.status(404).send('NOT FOUND');
+        }
+  };
 
-module.exports.funcstudentsstudentsIdPOST = function funcstudentsstudentsIdPOST(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for funcstudentsstudentsIdPOST'
-  });
-};
-'use strict'
 
-module.exports.funcstudentsstudentsIdPARAMETERS = function funcstudentsstudentsIdPARAMETERS(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for funcstudentsstudentsIdPARAMETERS'
-  });
-};
-
-module.exports.getStudents = function getStudents(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for getStudents'
-  });
-};
-
-module.exports.updateStudents = function updateStudents(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for updateStudents'
-  });
-};
-
-module.exports.deleteStudents = function deleteStudents(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for deleteStudents'
-  });
-};
-
-module.exports.funcstudentsstudentsIdPOST = function funcstudentsstudentsIdPOST(req, res, next) {
-  res.send({
-    message: 'This is the mockup controller for funcstudentsstudentsIdPOST'
-  });
-};
